e1d8f0c144e12fc57c0de454efb591880d0a6424
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-02-18 14:51:42 +0300

    custom django commands: change project name

--- src/core/management/commands/rename.py
+++ src/core/management/commands/rename.py
@@ -9,20 +9,20 @@ class Command(BaseCommand):
         parser.add_argument('new_project_name', type=str,
                             help='The new django project name')
 
-    def handle(self, *args, *kwargs):
+    def handle(self, *args, **kwargs):
         new_project_name = kwargs['new_project_name']
 
         files_to_remane = ['demo/settings/base.py',
                            'demo/wsgi.py', 'manage.py']
-        folder_to_remane = 'demo'
+        folder_to_rename = 'demo'
 
         for f in files_to_remane:
             with open(f, 'r') as file:
                 filedata = file.read()
 
-            filedata = filedata.replace('demo')
+            filedata = filedata.replace('demo', new_project_name)
             with open(f, 'w') as file:
                 file.write(filedata)
-        os.remane(folder_to_remame, new_project_name)
+        os.rename(folder_to_rename, new_project_name)
 
         self.stdout.write(self.style.SUCCESS('Project has been renamed'))
