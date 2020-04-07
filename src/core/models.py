c2e1479325927957784247bd324d3418dfc31f72
Abdi-Adan <53440762+Abdi-Adan@users.noreply.github.com>
2020-04-07 21:48:00 +0300

    setup .gitignore, updated readme, db flush

--- src/core/models.py
+++ src/core/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-# class Shirts(models.model):
-#     title = model.TextField(max_length=200)
-#     des = model.CharField()
-#     price = model.intergerFiled()
